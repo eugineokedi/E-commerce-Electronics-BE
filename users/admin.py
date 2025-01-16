36c96ffa276844754c6b6633819a0d7c5beb776c
Eugine Emoyo Okedi <okedieugine@gmail.com>
2025-01-16 22:25:11 +0300

    added signal

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Profile
 
 # Register your models here.
 
@@ -8,3 +8,9 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'email', 'date_joined')
     search_fields = ('email')
     ordering = ('-date_joined',)
+    
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):  
+    list_display = ('user')  
+  
---
+++ users/signal.py
@@ -0,0 +1,15 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from .models import User, Profile
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Automatically creates or updates the Profile when a User instance is saved.
+    """
+    if created:
+        # Create the profile when the user is created
+        Profile.objects.create(user=instance)
+    else:
+        # Save the profile when the user is updated
+        instance.profile.save()
